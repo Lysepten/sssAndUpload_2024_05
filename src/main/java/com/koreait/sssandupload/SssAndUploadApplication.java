1652475af4e277449857c0554290a41ff620fab1
Lysepten <tlstlsrbrb11@gmail.com>
2024-05-20 14:38:45 +0900

    추가 세팅

--- src/main/java/com/koreait/sssandupload/SssAndUploadApplication.java
+++ src/main/java/com/koreait/sssandupload/SssAndUploadApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SssAndUploadApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SssAndUploadApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SssAndUploadApplication.class, args);
+    }
 
 }
--- src/test/java/com/koreait/sssandupload/SssAndUploadApplicationTests.java
+++ src/test/java/com/koreait/sssandupload/SssAndUploadApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SssAndUploadApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+    @Test
+    void contextLoads() {
+    }
 
 }
